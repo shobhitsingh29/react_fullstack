f00e419eccb230f1ecbf456f5d227cccaf56da4f
shobhit-singh <shobhitsingh29@gmail.com>
2019-12-28 04:06:12 +0530

    Updates

---
+++ precache-manifest.c5ee97e18cc3df29cabc4d814fe90570.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "718079fdc22ae88c5c902fd0dd648268",
+    "url": "/react_fullstack/index.html"
+  },
+  {
+    "revision": "c9310633dd106cc72f00",
+    "url": "/react_fullstack/static/js/2.ce841361.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/react_fullstack/static/js/2.ce841361.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c14ac4ca6ee4429d8e99",
+    "url": "/react_fullstack/static/js/main.a3bed7a3.chunk.js"
+  },
+  {
+    "revision": "cc00b4a39c72ddb45ddb",
+    "url": "/react_fullstack/static/js/runtime-main.1d47a226.js"
+  }
+]);
